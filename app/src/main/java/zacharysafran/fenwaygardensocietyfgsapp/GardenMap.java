d470b60df2bc4f407288fe4b08651f3219ca6ac5
Zachary Safran <zsafran@bu.edu>
2017-01-10 13:36:03 -0500

    Pinch and Zoom Garden Map

--- app/src/main/java/zacharysafran/fenwaygardensocietyfgsapp/GardenMap.java
+++ app/src/main/java/zacharysafran/fenwaygardensocietyfgsapp/GardenMap.java
@@ -1,13 +1,21 @@
 package zacharysafran.fenwaygardensocietyfgsapp;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class GardenMap extends AppCompatActivity {
+
+ImageView mImageView;
+PhotoViewAttacher mAttacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,22 @@ public class GardenMap extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        // Any implementation of ImageView can be used!
+        mImageView = (ImageView) findViewById(R.id.image_view);
+
+        // Set the Drawable displayed
+        //Drawable bitmap = getResources().getDrawable(R.drawable.fenway_mapimageview);
+        //mImageView.setImageDrawable(bitmap);
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fenway_mapimageview);
+        mImageView.setImageDrawable(drawable);
+
+        // Attach a PhotoViewAttacher, which takes care of all of the zooming functionality.
+        // (not needed unless you are going to change the drawable later)
+        mAttacher = new PhotoViewAttacher(mImageView);
+
     }
+
+    //mAttacher.update();
 
 }
