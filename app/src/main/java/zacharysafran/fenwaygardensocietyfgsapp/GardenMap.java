0f5701419c37c412d045f69dcdfecac3a6d047f0
Zachary Safran <zsafran@bu.edu>
2017-01-10 11:37:44 -0500

    yeah this does not work

--- app/src/main/java/zacharysafran/fenwaygardensocietyfgsapp/GardenMap.java
+++ app/src/main/java/zacharysafran/fenwaygardensocietyfgsapp/GardenMap.java
@@ -1,13 +1,35 @@
 package zacharysafran.fenwaygardensocietyfgsapp;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class GardenMap extends AppCompatActivity {
+
+    ImageView mImageView;
+    PhotoViewAttacher mAttacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +38,19 @@ public class GardenMap extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        // Any implementation of ImageView can be used!
+        mImageView = (ImageView) findViewById(R.id.image_view);
+
+        // Set the Drawable displayed
+        Drawable bitmap = getResources().getDrawable(R.drawable.fenway_mapimageview);
+        mImageView.setImageDrawable(bitmap);
+
+        // Attach a PhotoViewAttacher, which takes care of all of the zooming functionality.
+        // (not needed unless you are going to change the drawable later)
+        mAttacher = new PhotoViewAttacher(mImageView);
     }
 
-}
+// If you later call mImageView.setImageDrawable/setImageBitmap/setImageResource/etc then you just need to call
+    //mAttacher.update();
+
+    }
