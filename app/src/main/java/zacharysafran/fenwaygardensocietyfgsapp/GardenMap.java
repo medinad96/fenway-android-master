10bce8869a40f07682fceb7445180bbf9d3162c1
Zachary Safran <zsafran@bu.edu>
2017-01-10 19:30:00 -0500

    Pinch and Zoom Garden Map -added floating back button

--- app/src/main/java/zacharysafran/fenwaygardensocietyfgsapp/GardenMap.java
+++ app/src/main/java/zacharysafran/fenwaygardensocietyfgsapp/GardenMap.java
@@ -24,14 +24,14 @@ PhotoViewAttacher mAttacher;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
 
         // Any implementation of ImageView can be used!
         mImageView = (ImageView) findViewById(R.id.image_view);
